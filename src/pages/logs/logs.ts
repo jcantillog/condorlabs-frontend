45e28e564b8c1bef7a044fd742d273e1bd5237b8
jcantillog <jcantillog@unicartagena.edu.co>
2018-01-19 19:18:46 -0500

    Ordered by Start Log Date by default

--- src/pages/logs/logs.ts
+++ src/pages/logs/logs.ts
@@ -20,7 +20,8 @@ export class LogsPage implements OnInit{
   }
 
   ngOnInit(){
-      this.getLogs();
+      this.getLogsTest();
+      this.sortBy('Start Log Date');
   }
 
   setPage(page: number) {
@@ -50,6 +51,90 @@ export class LogsPage implements OnInit{
                   console.log(error);
               }
           );
+  }
+  getLogsTest(){
+      this.logsList.push(new LogModel("c", "", "",
+          "", "", "2017-01-17T03:24:00", "",
+          "2017-01-17T04:24:00", "", "", 1));
+      this.logsList.push(new LogModel("a", "", "",
+          "", "", "2017-01-18T03:24:00", "",
+          "2017-01-18T04:24:00", "", "", 2));
+      this.logsList.push(new LogModel("b", "", "",
+          "", "", "2017-01-19T03:24:00", "",
+          "2017-01-19T04:24:00", "", "", 0));
+      this.setPage(1);
+  }
+
+  sortBy(field: string){
+      const loader = this.loadingCtrl.create({
+          content: "Sorting by " + field + "..."
+      });
+      loader.present();
+      switch (field) {
+          case 'State Code':
+              this.logsList.sort((a, b) => {
+                  return a.cd_cebroker_state.localeCompare(b.cd_cebroker_state);
+              });
+              break;
+          case 'Pro Code':
+              this.logsList.sort((a, b) => {
+                  return a.pro_cde - b.pro_cde;
+              });
+              break;
+          case 'Profession':
+              this.logsList.sort((a, b) => {
+                  return a.cd_profession.localeCompare(b.cd_profession);
+              });
+              break;
+          case 'License ID':
+              this.logsList.sort((a, b) => {
+                  return a.id_license.localeCompare(b.id_license);
+              });
+              break;
+          case 'Cycle End Date':
+              this.logsList.sort((a, b) => {
+                  let date_one = new Date(a.dt_end);
+                  let date_two = new Date(b.dt_end);
+                  return date_two > date_one ? 1 : date_two < date_one ? -1 : 0;
+              });
+              break;
+          case 'Compliance Status':
+              this.logsList.sort((a, b) => {
+                  return a.ds_compl_status_returned.localeCompare(b.ds_compl_status_returned);
+              });
+              break;
+          case 'Client ID':
+              this.logsList.sort((a, b) => {
+                  return a.id_client_nbr.localeCompare(b.id_client_nbr);
+              });
+              break;
+          case 'Start Log Date':
+              this.logsList.sort((a, b) => {
+                  let date_one = new Date(a.dt_Start_Log);
+                  let date_two = new Date(b.dt_Start_Log);
+                  return date_two > date_one ? 1 : date_two < date_one ? -1 : 0;
+              });
+              break;
+          case 'End Log Date':
+              this.logsList.sort((a, b) => {
+                  let date_one = new Date(a.dt_end_log);
+                  let date_two = new Date(b.dt_end_log);
+                  return date_two > date_one ? 1 : date_two < date_one ? -1 : 0;
+              });
+              break;
+          case 'Environment':
+              this.logsList.sort((a, b) => {
+                  return a.cd_environment.localeCompare(b.cd_environment);
+              });
+              break;
+          case 'Machine':
+              this.logsList.sort((a, b) => {
+                  return a.cd_machine.localeCompare(b.cd_machine);
+              });
+              break;
+      }
+      loader.dismiss();
+      this.setPage(1);
   }
 
   getCurrentDate(){
